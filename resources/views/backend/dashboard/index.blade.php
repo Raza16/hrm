1ab37388b160dc33756108fe7a86626517d574be
Raza16 <raza@datech.com>
2021-02-18 20:18:13 +0500

    modified

--- resources/views/backend/dashboard/index.blade.php
+++ resources/views/backend/dashboard/index.blade.php
@@ -186,11 +186,6 @@
                 <div class="card">
                     <div class="card-header">
                         <h3 class="card-title">Project Statistics</h3>
-                        {{-- <div class="card-options">
-                            <a href="#" class="card-options-collapse" data-toggle="card-collapse"><i class="fe fe-chevron-up"></i></a>
-                            <a href="#" class="card-options-fullscreen" data-toggle="card-fullscreen"><i class="fe fe-maximize"></i></a>
-                            <a href="#" class="card-options-remove" data-toggle="card-remove"><i class="fe fe-x"></i></a>
-                        </div> --}}
                     </div>
                     <div class="card-body">
                         <div class="row text-center">
