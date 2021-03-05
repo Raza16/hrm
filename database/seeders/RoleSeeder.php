c10533ce020e8d767b0ec67453a7f79ea18fd6cb
Raza16 <raza@datech.com>
2021-03-05 21:13:15 +0500

    new files

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use DB;
+use Carbon\Carbon;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $roles = [
+            [
+                'role_type' => 'Admin',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+            [
+                'role_type' => 'Employee',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+        ];
+
+        DB::table('roles')->insert($roles);
+
+    }
+}
