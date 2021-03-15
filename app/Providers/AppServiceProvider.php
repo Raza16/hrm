f3990ff310efed6c97256770598c1ce2c09c263d
Raza16 <raza@datech.com>
2021-03-15 19:41:02 +0500

    modified

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,7 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
-       Paginator::useBootstrap();
+        Builder::defaultStringLength(191);
+        Paginator::useBootstrap();
     }
 }
