2822d9736d479b4238a3453db896df92f026ce95
Raza16 <raza@datech.com>
2021-03-27 14:21:35 +0500

    modified

--- resources/views/backend/user_account/dashboard.blade.php
+++ resources/views/backend/user_account/dashboard.blade.php
@@ -459,11 +459,11 @@
 
 
 
-tElementById('ct6').innerHTML = x1;
-        display_c6();
-        }
-        function display_c6(){
-        var refresh=1000; // Refresh rate in milli seconds
-        mytime=setTimeout('display_ct6()',refresh)
-        }
-    display_c6()
+// tElementById('ct6').innerHTML = x1;
+//         display_c6();
+//         }
+//         function display_c6(){
+//         var refresh=1000; // Refresh rate in milli seconds
+//         mytime=setTimeout('display_ct6()',refresh)
+//         }
+//     display_c6()
